8eba7f8a6f0097eb39d28f3e85e221d1ccb33546
robertmitchellv <robert.beaty.mitchell@gmail.com>
2024-09-30 13:35:43 -0700

    update version of IG used

--- validate.sh
+++ validate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 java -jar validator_cli.jar "$1" \
-  -ig hl7.fhir.us.ecr\#2.1.1 \
+  -ig hl7.fhir.us.ecr\#2.1.2 \
   -version 4.0.1 \
   -output validation_output.xml \
   -html-output validation_output.html \
